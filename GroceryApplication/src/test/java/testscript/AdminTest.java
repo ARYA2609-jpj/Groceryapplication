ed6b326d45490d1f71718e3933ff2ee48a499f79
Arya <aryasudarsanan2022@gmail.com>
2025-09-11 18:54:45 +0530

    This is a samples of automation project

--- GroceryApplication/src/test/java/testscript/AdminTest.java
+++ GroceryApplication/src/test/java/testscript/AdminTest.java
@@ -23,68 +23,79 @@ import utilities.FakerUtility;
 
 public class AdminTest extends TestNgBase {
 	@Test(description = "Verify add user")
-	public void verifyadduser() throws IOException{
-		String usernameValue=ExcelUtility.getStringData(1, 0,Constants.LOGINSHEET );
-		String passwordValue=ExcelUtility.getStringData(1, 1, Constants.LOGINSHEET);
-		LoginPage loginpage=new LoginPage(driver);
-		loginpage.enterusername(usernameValue);
-		loginpage.enterpassword(passwordValue);
-		loginpage.signin();
-		
+	public void verifyadduser() throws IOException {
+        String usernameValue = ExcelUtility.getStringData(1, 0, Constants.LOGINSHEET);
+        String passwordValue = ExcelUtility.getStringData(1, 1, Constants.LOGINSHEET);
+        LoginPage loginpage = new LoginPage(driver);
+        loginpage.enterusername(usernameValue);
+        loginpage.enterpassword(passwordValue);
+        loginpage.signin();
 
-		FakerUtility fakerUtility = new FakerUtility();
-		String name=fakerUtility.createRandomUserName();
-		String password=fakerUtility.createRandomPassword();
-		String userTypevalue=ExcelUtility.getStringData(1, 2, Constants.ADMINSHEET);
-		AdminPage adminpage=new AdminPage(driver);
-		adminpage.clickadmintile();
-		adminpage.clicknewbutton();
-		adminpage.enterusername(usernameValue);
-		adminpage.enterpassword(passwordValue);
-		adminpage.selectusertype(userTypevalue);
-		adminpage.clicksavebutton();
-		
+        FakerUtility fakerUtility = new FakerUtility();
+        String newUser = fakerUtility.createRandomUserName();
+        String newPassword = fakerUtility.createRandomPassword();
+        String userTypevalue = ExcelUtility.getStringData(1, 2, Constants.ADMINSHEET);
 
+        AdminPage adminpage = new AdminPage(driver);
+        adminpage.clickadmintile();
+        adminpage.clicknewbutton();
+        adminpage.enterusername(newUser);
+        adminpage.enterpassword(newPassword);
+        adminpage.selectusertype(userTypevalue);
+        adminpage.clicksavebutton();
 
+        Assert.assertTrue(adminpage.isAlertDisplayed(), "Unable to add User");
+        System.out.println("Add user alert: " + adminpage.getAlertMessage());
+    }
 
-	}
-	@Test(description = "Verify search user")
-	public void verifysearch() throws IOException{
-		String usernameValue=ExcelUtility.getStringData(1, 0,Constants.LOGINSHEET );
-		String passwordValue=ExcelUtility.getStringData(1, 1, Constants.LOGINSHEET);
-		LoginPage loginpage=new LoginPage(driver);
-		loginpage.enterusername(usernameValue);
-		loginpage.enterpassword(passwordValue);
-		loginpage.signin();
-		
+    @Test(description = "Verify search user")
+    public void verifysearch() throws IOException {
+        String usernameValue = ExcelUtility.getStringData(1, 0, Constants.LOGINSHEET);
+        String passwordValue = ExcelUtility.getStringData(1, 1, Constants.LOGINSHEET);
+        LoginPage loginpage = new LoginPage(driver);
+        loginpage.enterusername(usernameValue);
+        loginpage.enterpassword(passwordValue);
+        loginpage.signin();
 
-		FakerUtility fakerUtility = new FakerUtility();
-		String name=fakerUtility.createRandomUserName();
-		String password=fakerUtility.createRandomPassword();
-		String userTypevalue=ExcelUtility.getStringData(1, 2, Constants.ADMINSHEET);
-		AdminPage adminpage=new AdminPage(driver);
-		adminpage.clickadmintile();
-		adminpage.clicksearchbutton();
-		adminpage.entersearchusername(name);
-		adminpage.selectsearchusertype(userTypevalue);
-		adminpage.clicksearchfield();
-		
-	}
-	@Test(description = "Verify home page")
-	public void verifyhomepage() throws IOException{
-		String usernameValue=ExcelUtility.getStringData(1, 0,Constants.LOGINSHEET );
-		String passwordValue=ExcelUtility.getStringData(1, 1, Constants.LOGINSHEET);	
-		LoginPage loginpage=new LoginPage(driver);
-		loginpage.enterusername(usernameValue);
-		loginpage.enterpassword(passwordValue);
-		loginpage.signin();
-		
+        FakerUtility fakerUtility = new FakerUtility();
+        String newUser = fakerUtility.createRandomUserName();
+        String newPassword = fakerUtility.createRandomPassword();
+        String userTypevalue = ExcelUtility.getStringData(1, 2, Constants.ADMINSHEET);
 
-		AdminPage adminpage = new AdminPage(driver);
-		adminpage.clicknewstile();
-		adminpage.clickhome();
-		
+        AdminPage adminpage = new AdminPage(driver);
+        adminpage.clickadmintile();
 
-	}
+        
+        adminpage.clicknewbutton();
+        adminpage.enterusername(newUser);
+        adminpage.enterpassword(newPassword);
+        adminpage.selectusertype(userTypevalue);
+        adminpage.clicksavebutton();
+        Assert.assertTrue(adminpage.isAlertDisplayed(), "Unable to add User before search");
 
+        
+        adminpage.clicksearchbutton();
+        adminpage.entersearchusername(newUser);
+        adminpage.selectsearchusertype(userTypevalue);
+        adminpage.clicksearchfield();
+
+        boolean isUserFound = adminpage.isUserPresentInSearchResults(newUser);
+        Assert.assertTrue(isUserFound, "User not found in search results: " + newUser);
+    }
+
+    @Test(description = "Verify home page")
+    public void verifyhomepage() throws IOException {
+        String usernameValue = ExcelUtility.getStringData(1, 0, Constants.LOGINSHEET);
+        String passwordValue = ExcelUtility.getStringData(1, 1, Constants.LOGINSHEET);
+        LoginPage loginpage = new LoginPage(driver);
+        loginpage.enterusername(usernameValue);
+        loginpage.enterpassword(passwordValue);
+        loginpage.signin();
+
+        AdminPage adminpage = new AdminPage(driver);
+        adminpage.clicknewstile();
+        adminpage.clickhome();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("home"), "Not redirected to home page");
+    }
 }
